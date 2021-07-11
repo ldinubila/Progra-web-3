cda664818f6a6288a1010747dfb0a6072736fa78
PerlaMoly <pengate@gmail.com>
2021-07-10 21:21:05 -0300

    Ajustes en login

--- Progra-web-3_Tp-final/Controllers/HomeController.cs
+++ Progra-web-3_Tp-final/Controllers/HomeController.cs
@@ -7,16 +7,30 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Progra_web_3_Tp_final.Servicios;
+using System.Security.Claims;
+
+
 
 namespace Progra_web_3_Tp_final.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly LoginServicio _loginServicio;
+        private readonly TokenServicio _tokenServicio;
+        private readonly IConfiguration _configuration;
+        public HomeController(ILogger<HomeController> logger, IConfiguration config)
         {
             _logger = logger;
+            _20211CTPContext dbContext = new _20211CTPContext();
+            _configuration = config;
+            _loginServicio = new LoginServicio(dbContext);
+            _tokenServicio = new TokenServicio();
         }
 
         public IActionResult Index()
@@ -37,6 +51,60 @@ namespace Progra_web_3_Tp_final.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-       
+        public IActionResult Ingresar()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public async System.Threading.Tasks.Task<IActionResult> IngresarAsync(string mail, string pass)
+        {
+            _loginServicio.Ingresar(mail, pass, out Usuario usuarioSalida);
+            if (usuarioSalida != null)
+            {
+                string tokengenerado = _tokenServicio.generarToken(usuarioSalida, _configuration);
+                HttpContext.Session.SetString("Token", tokengenerado);
+                var VistaAnteriorSinLogin = HttpContext.Session.GetString("VistaAnteriorSinLogin");
+
+                //var claims = new List<Claim>
+                //{
+                //    new Claim(ClaimTypes.Name, usuarioSalida.Nombre),
+                //    new Claim("username", usuarioSalida.Email),
+                //    new Claim(ClaimTypes.NameIdentifier, usuarioSalida.IdUsuario.ToString()),
+                //    new Claim(ClaimTypes.Role, usuarioSalida.EsAdmin ? "Admin" : "Usuario")
+                //};
+
+                //var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                //HttpContext.SignInAsync(new ClaimsPrincipal(claimsIdentity));
+
+                if (VistaAnteriorSinLogin == null)
+                    return Redirect("/Pedidos");
+                else
+                    return Redirect(VistaAnteriorSinLogin);
+            }
+            else
+            {
+                TempData["Error"] = "Error. Email o contraseña inválidas";
+
+                return Redirect("/Home");
+            }
+
+        }
+
+
+        public IActionResult Logout()
+        {
+
+            HttpContext.SignOutAsync();
+            return Redirect("/Login");
+
+        }
+
+
+
     }
+
+
 }
--- Progra-web-3_Tp-final/Startup.cs
+++ Progra-web-3_Tp-final/Startup.cs
@@ -1,10 +1,15 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
+using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -23,6 +28,34 @@ namespace Progra_web_3_Tp_final
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSession();
+            var key = Encoding.ASCII.GetBytes(Configuration.GetValue<string>("SecretKey"));
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+            services.AddMvc();
+
+            /*services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
+            {
+                //   ...
+            });  //PLM*/
+
+            /*
             services.AddControllersWithViews();
             //En este caso se usa la memoria del propio servidor (ver si se puede cambiar).
             services.AddDistributedMemoryCache();
@@ -30,7 +63,7 @@ namespace Progra_web_3_Tp_final
             {
                 options.Cookie.Name = ".MiAPP.Session";
                 options.IdleTimeout = TimeSpan.FromSeconds(60);
-            });
+            });*/
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -70,7 +103,7 @@ namespace Progra_web_3_Tp_final
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Ingresar}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Ingresar}/{id?}");
             });
         }
     }
